4c9e05fd62d162bd0fb8aba6f7739dc39f75cad6
szrharrison <s.z.r.harrison@gmail.com>
2017-05-14 15:13:21 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 //Code React element here
+const title = React.createElement('h1', {}, 'An Awesome Person')
+const description = React.createElement('p', {}, 'Who is learning React')
+
+const interestsStrings = ['JavaScript', 'React', 'Movies', 'Ice cream']
+const interestsLIs = interestsStrings.map( string => React.createElement('li', {}, string) )
+const interests = React.createElement('ul', { className: 'me__interests' }, interestsLIs)
+
+const meInReact = React.createElement('div', { className: 'me' }, [
+  title,
+  description,
+  interests
+])
+
+ReactDOM.render(meInReact, document.getElementById('main'))
